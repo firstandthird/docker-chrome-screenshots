adb80c640e0f62b6b34a64351bb3133ba9b13c8b
Eric Willis <eric@ecwillis.com>
2017-05-05 20:01:55 +0000

    Adding route config and testing

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,39 @@
 const crypto = require('crypto');
 const exec = require('child_process').exec;
+const Joi = require('joi');
 
-const md5 = function(str) {
-  return crypto.createHash('md5').update(str).digest('hex');
+const rando = function(len) {
+  return crypto.randomBytes(Math.ceil(len/2)).toString('hex').slice(0,len);
 };
 
 exports.main = {
   path: '/',
   method: 'GET',
+  config: {
+    validate: {
+      query: {
+        url: Joi.string().required(),
+        viewport: Joi.string().optional()
+      }
+    }
+  },
   handler(request, reply) {
     const url = request.query.url;
-    const hid = md5(request.query.url);
-    const dir = `./screenshots/${hid}`;
-    exec(`mkdir -p ${dir} && cd ${dir} && google-chrome --headless --disable-gpu --screenshot ${url}`, (err, stdout, stderr) => {
+    const randFldr = rando(18);
+    const dir = `./screenshots/${randFldr}`;
+
+    const command = [];
+
+    command.push(`mkdir -p ${dir} && cd ${dir} &&`);
+    command.push('google-chrome --headless --disable-gpu --screenshot');
+
+    if (request.query.viewport) {
+      command.push(`--window-size=${request.query.viewport}`);
+    }
+
+    command.push(url);
+
+    exec(command.join(" "), (err, stdout, stderr) => {
       console.log(stdout);
       reply.file(`${dir}/screenshot.png`);
     });
